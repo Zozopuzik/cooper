53064af488e513d09e5558991bb25cc0b87e51b4
zozopuzik <evgen.sabadash1337@gmail.com>
2023-10-15 00:32:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Contacts from './components/Contacts/Contacts';
+import Header from './components/Header/Header';
+import Slider from './components/Slider/Slider';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Slider />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
